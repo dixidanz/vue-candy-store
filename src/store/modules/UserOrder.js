2702353c003dae0ea94b156b53078f15ef053c47
Dan <gbce1234@gmail.com>
2018-11-19 20:05:07 +0800

    order pay

--- src/store/modules/UserOrder.js
+++ src/store/modules/UserOrder.js
@@ -8,8 +8,12 @@ export default ({
   strict: true,
   namespaced: true,
   state: {
+    order: {}
   },
   mutations: {
+    getOrder: (state, order) => {
+      state.order = order
+    }
   },
   actions: {
     ORDER_CREATE: ({ commit }, user) => {
@@ -21,6 +25,24 @@ export default ({
           if (!res.data.success) return false
           resolve(res.data)
         })
+      })
+    },
+    ORDER_GET: ({ commit }, id) => {
+      commit('updateLoading', true, { root: true })
+      const api = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_CUSTOMPATH}/order/${id}`
+      axios.get(api).then(res => {
+        commit('updateLoading', false, { root: true })
+        if (!res.data.success) return false
+        commit('getOrder', res.data.order)
+      })
+    },
+    ORDER_PAY: ({ commit, dispatch }, id) => {
+      commit('updateLoading', true, { root: true })
+      const api = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_CUSTOMPATH}/pay/${id}`
+      axios.post(api, id).then(res => {
+        commit('updateLoading', false, { root: true })
+        if (!res.data.success) return false
+        dispatch('ORDER_GET', id)
       })
     }
   },
--- src/views/User/Checkout/index.vue
+++ src/views/User/Checkout/index.vue
@@ -1,6 +1,22 @@
 <script>
 export default {
-  name: 'checkout'
+  name: 'checkout',
+  computed: {
+    orderId() {
+      return this.$route.params.orderId
+    },
+    order() {
+      return this.$store.state.UserOrderModules.order
+    }
+  },
+  methods: {
+    payOrder() {
+      this.$store.dispatch('UserOrderModules/ORDER_PAY', this.orderId)
+    }
+  },
+  mounted() {
+    this.$store.dispatch('UserOrderModules/ORDER_GET', this.orderId)
+  }
 }
 </script>
 
