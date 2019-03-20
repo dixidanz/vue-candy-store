e46c13f3211c6d3679b529330937ea3749db96ac
Dan <gbce1234@gmail.com>
2019-03-20 18:31:41 +0800

    product card

--- src/views/User/Products/index.vue
+++ src/views/User/Products/index.vue
@@ -1,37 +1,37 @@
 <script>
-import Carousel from '@/components/Carousel/index';
-import Categories from '@/components/Categories/index';
-import ProductList from '@/components/ProductList/index';
-import CartList from '@/components/CartList/index';
-import CartIcon from '@/components/CartIcon/index';
-// import Pagination from '@/components/Pagination/index';
+  import Carousel from '@/components/Carousel/index';
+  import Categories from '@/components/Categories/index';
+  import ProductCard from '@/components/ProductCard/index';
+  import CartList from '@/components/CartList/index';
+  import CartIcon from '@/components/CartIcon/index';
+  // import Pagination from '@/components/Pagination/index';
 
-import { mapGetters } from 'vuex'
-export default {
-  name: 'products',
-  components: {
-    Carousel,
-    Categories,
-    ProductList,
-    CartList,
-    CartIcon,
-    // Pagination
-  },
-  computed: {
-    ...mapGetters('UserProductsModules', [
-      'categories',
-      'products',
-      // 'pagination'
-    ]),
-    ...mapGetters('UserCartModules', [
-      'cartCount'
-    ])
-  },
-  mounted() {
-    this.$store.dispatch('UserProductsModules/PRODUCTS_GET')
-    this.$store.dispatch('UserCartModules/CARTS_GET')
+  import { mapGetters } from 'vuex'
+  export default {
+    name: 'products',
+    components: {
+      Carousel,
+      Categories,
+      ProductCard,
+      CartList,
+      CartIcon,
+      // Pagination
+    },
+    computed: {
+      ...mapGetters('UserProductsModules', [
+        'categories',
+        'products',
+        // 'pagination'
+      ]),
+      ...mapGetters('UserCartModules', [
+        'cartCount'
+      ])
+    },
+    mounted() {
+      this.$store.dispatch('UserProductsModules/PRODUCTS_GET')
+      this.$store.dispatch('UserCartModules/CARTS_GET')
+    }
   }
-}
 </script>
 
 <template src="./template.html"></template>
